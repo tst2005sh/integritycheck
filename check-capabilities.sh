c36993244ee9f31a88e65a784458a0de324fe141
TsT <tst2005@gmail.com>
2024-05-22 10:36:18 +0200

    updated check-capabilites

--- check-capabilities.sh
+++ check-capabilities.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+cd "$(dirname "$0")" || exit 1
+
+tmp_ignore_fgrep="$(mktemp)"
+trap "rm -f -- '$tmp_ignore_fgrep'" EXIT
+
+if [ -f ./assumeok.capabilites.fgrep ]; then
+	cat ./assumeok.capabilites.fgrep > "$tmp_ignore_fgrep"
+else
+	echo '#' > "$tmp_ignore_fgrep"
+fi
+
+okdirs() {
+	grep -h '^[^#]\+' "$tmp_ignore_fgrep" | rev |cut -d/ -f2- |rev |sort -u
+}
+
 QUICK=false
 while [ $# -gt 0 ]; do
 	case "$1" in
@@ -11,23 +26,27 @@ while [ $# -gt 0 ]; do
 	shift
 done
 
-okdirs() {
-	cat assumeok.capabilites.fgrep | rev |cut -d/ -f2- |rev |sort -u
-}
-
 if ${QUICK:-false}; then
 	set -- $(okdirs)
 fi
+
+if [ $# -eq 0 ]; then
+	set -- /bin /sbin /usr/bin /usr/sbin /lib* /usr/lib*
+fi
+
+#echo "args: $#: $*"
 
 if ! command -v getcap >/dev/null 2>&1 && [ "$(id -u)" != 0 ]; then
 	echo >&2 "$0 is supposed to be run as root"
 	exit 1
 fi
 
-if [ $# -eq 0 ]; then
-	set -- /bin /sbin /usr/bin /usr/sbin /lib* /usr/lib*
-fi
+# try to ouput debian 9 getcap like the one of debian 10-12
+debian9_forwardcompatility() {
+	sed -E -e 's, = ([^+]+)\+([^+]+)$, \1=\2,g'
+}
+
 getcap -r "$@" |
-sed -E -e 's, = ([^+]+)\+([^+]+)$, \1=\2,g' |
-grep -vFx -f assumeok.capabilites.fgrep ||
+debian9_forwardcompatility |
+grep -vFx -f "$tmp_ignore_fgrep" ||
 echo >&2 "ok"
