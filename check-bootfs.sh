fb81c1814aad06b981194f070157f67826f6f462
TsT <tst2005@gmail.com>
2024-07-02 18:21:16 +0200

    check-bootfs improved

--- check-bootfs.sh
+++ check-bootfs.sh
@@ -16,6 +16,13 @@ while [ $# -gt 0 ]; do
 	shift
 done
 
+#### CURRENT-FILES ####
+( cd /boot && find ./ -type f -exec sha1sum {} \; ) |
+sort -u -t\  -k3 > $installed
+#### /CURRENT-FILES ####
+
+
+#### SAFE-FILES ####
 {
 	# get /boot/config-* /boot/System.map-* /boot/vmlinuz-* from linux-image-* packages
 	dpkg-query -L $(
@@ -24,24 +31,39 @@ done
 	sort -u |
 	grep '^/boot'
 
-	# search all files from grub-* and grub2-*
+	# grub background png
+	dpkg 2>&- -L desktop-base |
+	grep '/grub.*\.png$'|
+	sort -u
+
 	dpkg-query -L $(
-		dpkg -l |cut -c5- |grep -E '^grub(|2)-'|cut -d\  -f1
+		{
+			# include all content of grub packages
+			dpkg -l |cut -c5- |grep -E '^(grub|grub2)-'|cut -d\  -f1
+
+			# include all content of packages that provide any .efi files
+			find /usr/ -type f -print0 |
+			grep --null-data -E '\.efi(|\.signed)$' |
+			sed --null-data -e 's,^/usr,,g' -e 'p;s,^,/usr,g' |
+			xargs -0 dpkg-query -S 2>/dev/null |
+			cut -d: -f1
+		} |
+		sort -u
 	) |
 	sort -u
 } |
+sort -u |
 while read -r f; do
 	[ -f "$f" ] || continue
 	printf %s\\n "$f"
 done |
+#### /SAFE-FILES ####
+#### SAFE-CHECKSUM ####
 xargs sha1sum -z |
 cut -z -d\  -f1 |
 sed -z -e 's,^,^,g' |
 tr \\0 \\n > $safe
-
-( cd /boot && find ./ -type f -exec sha1sum {} \; ) |
-sort -u -t\  -k3 > $installed
-
+#### /SAFE-CHECKSUM ####
 
 grep -f $safe $installed > $stats
 stats_ok="$(cat $stats |wc -l |cut -d\  -f1)"
@@ -58,4 +80,3 @@ sed -E  -e 's,^([^ ]+  )\./,\1/boot/,g' -e 's,^,unknown:,g'
 stats_ko="$(cat $stats |wc -l |cut -d\  -f1)"
 
 echo >&2 "Stats: $stats_ko unknown / $(( $stats_ok + $stats_ko )) total"
-
