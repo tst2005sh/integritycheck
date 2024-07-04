1059852e7ee17defee4ad2eeacf2552f93c121c8
TsT <tst2005@gmail.com>
2024-07-04 15:46:21 +0200

    Âcheck-debsums improved

--- check-debsums.sh
+++ check-debsums.sh
@@ -1,4 +1,13 @@
 #/bin/bash
 cd "$(dirname "$0")" || exit 1
+
+if ! command -v debsums >/dev/null 2>&1; then
+	echo >&2 'debsums command not found (consider to apt-get install debsums)'
+	exit 1
+fi
 debsums -a -s 2>&1 |
-grep -vFx -f assumeok.debsums.fgrep || echo >&2 "ok"
+if [ -f assumeok.debsums.fgrep ]; then
+	grep -vFx -f assumeok.debsums.fgrep || echo >&2 "ok"
+else
+	cat
+fi
